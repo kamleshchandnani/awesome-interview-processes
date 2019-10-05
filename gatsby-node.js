6d061cc01cc86aa6636c7064710ac7a74a000768
kamleshchandnani <kamlesh.chandnani@gmail.com>
2019-10-05 09:21:56 +0530

    fix(posts): modify posts sorting order

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,97 +1,36 @@
-const withDefaults = require(`gatsby-theme-blog-core/utils/default-options`);
-const crypto = require(`crypto`);
-
-const mdxResolverPassthrough = fieldName => async (source, args, context, info) => {
-  const type = info.schema.getType(`Mdx`);
-  const mdxNode = context.nodeModel.getNodeById({
-    id: source.parent
-  });
-  const resolver = type.getFields()[fieldName].resolve;
-  const result = await resolver(mdxNode, args, context, {
-    fieldName
-  });
-  return result;
-};
-
 exports.createSchemaCustomization = ({ actions, schema }) => {
   const { createTypes } = actions;
-  createTypes(`type BlogPost implements Node @nodeInterface {
+  const typeDefs = `
+    type BlogPost implements Node @nodeInterface {
       date: Date @dateformat
+    }
+    type MdxBlogPost implements Node @infer {
+      fields: MdxBlogPostFields
+    }
+    type MdxBlogPostFields @infer {
       index: Int
-  }`);
-
+    }
+  `;
+  createTypes(typeDefs);
   createTypes(
     schema.buildObjectType({
       name: `MdxBlogPost`,
       fields: {
-        date: { type: `Date`, extensions: { dateformat: {} } },
-        index: { type: `Int` }
+        date: { type: `Date`, extensions: { dateformat: {} } }
       },
       interfaces: [`Node`, `BlogPost`]
     })
   );
 };
 
-// exports.onCreateNode = async ({ node, actions, getNode, createNodeId }, themeOptions) => {
-//   const { createNode, createParentChildLink } = actions;
-//   const { contentPath, basePath } = withDefaults(themeOptions);
-
-//   // Make sure it's an MDX node
-//   if (node.internal.type !== `Mdx`) {
-//     return;
-//   }
-
-//   // Create source field (according to contentPath)
-//   const fileNode = getNode(node.parent);
-//   const source = fileNode.sourceInstanceName;
-//   console.log("######from shadowed file", { type: node.internal.type, source, contentPath });
-//   if (node.internal.type === `Mdx` && source === "content/companies") {
-//     // let slug;
-//     // if (node.frontmatter.slug) {
-//     //   if (path.isAbsolute(node.frontmatter.slug)) {
-//     //     // absolute paths take precedence
-//     //     slug = node.frontmatter.slug;
-//     //   } else {
-//     //     // otherwise a relative slug gets turned into a sub path
-//     //     slug = urlResolve(basePath, node.frontmatter.slug);
-//     //   }
-//     // } else {
-//     //   // otherwise use the filepath function from gatsby-source-filesystem
-//     //   const filePath = createFilePath({
-//     //     node: fileNode,
-//     //     getNode,
-//     //     basePath: contentPath
-//     //   });
-
-//     //   slug = urlResolve(basePath, filePath);
-//     // }
-
-//     const fieldData = {
-//       index: node.frontmatter.index
-//       // title: node.frontmatter.title,
-//       // tags: node.frontmatter.tags || [],
-//       // slug,
-//       // date: node.frontmatter.date,
-//       // keywords: node.frontmatter.keywords || []
-//     };
-//     console.log("######from shadowed file", fieldData);
-//     const mdxBlogPostId = createNodeId(`${node.id} >>> Shadowed MdxBlogPost`);
-//     await createNode({
-//       ...fieldData,
-//       // Required fields.
-//       id: node.frontmatter.index,
-//       parent: node.id,
-//       children: [],
-//       internal: {
-//         type: `MdxBlogPost`,
-//         contentDigest: crypto
-//           .createHash(`md5`)
-//           .update(JSON.stringify(fieldData))
-//           .digest(`hex`),
-//         content: JSON.stringify(fieldData),
-//         description: `Mdx implementation of the BlogPost interface`
-//       }
-//     });
-//     createParentChildLink({ parent: node, child: getNode(mdxBlogPostId) });
-//   }
-// };
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions;
+  if (node.internal.type === `MdxBlogPost`) {
+    const parent = getNode(node.parent);
+    createNodeField({
+      node,
+      name: "index",
+      value: parent.frontmatter.index
+    });
+  }
+};
--- src/gatsby-theme-blog/components/posts.js
+++ src/gatsby-theme-blog/components/posts.js
@@ -1,45 +1,84 @@
 import React, { Fragment } from "react";
 import { Link } from "gatsby";
 import { Styled, css } from "theme-ui";
-
+import { useStaticQuery, graphql } from "gatsby";
 import Layout from "gatsby-theme-blog/src/components/layout";
 import SEO from "gatsby-theme-blog/src/components/seo";
 import Footer from "gatsby-theme-blog/src/components/home-footer";
 
-const Posts = ({ location, posts, siteTitle, socialLinks }) => (
-  <Layout location={location} title={siteTitle}>
-    <main>
-      {posts.map(({ node }) => {
-        const title = node.title || node.slug;
-        const keywords = node.keywords || [];
-        return (
-          <Fragment key={node.slug}>
-            <SEO title="Home" keywords={keywords} />
-            <div>
-              <Styled.h2
-                css={css({
-                  mb: 1
-                })}
-              >
-                <Styled.a
-                  as={Link}
-                  css={{
-                    textDecoration: `none`
-                  }}
-                  to={node.slug}
+const PostsQuery = graphql`
+  query PostsQueryShadowed {
+    site {
+      siteMetadata {
+        title
+        social {
+          name
+          url
+        }
+      }
+    }
+    allBlogPost(sort: { fields: [date, title], order: DESC }, limit: 1000) {
+      edges {
+        node {
+          id
+          excerpt
+          slug
+          title
+          ... on MdxBlogPost {
+            fields {
+              index
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+const sortPostsByIndex = (currentPost, nextPost) =>
+  currentPost.node.fields.index - nextPost.node.fields.index;
+
+const Posts = ({ location }) => {
+  const results = useStaticQuery(PostsQuery);
+  const posts = results.allBlogPost.edges;
+  const siteTitle = results.site.siteMetadata.title;
+  const socialLinks = results.site.siteMetadata.social;
+  const sortedPosts = posts.sort(sortPostsByIndex);
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <main>
+        {sortedPosts.map(({ node }) => {
+          const title = node.title || node.slug;
+          const keywords = node.keywords || [];
+          return (
+            <Fragment key={node.slug}>
+              <SEO title="Home" keywords={keywords} />
+              <div>
+                <Styled.h2
+                  css={css({
+                    mb: 1
+                  })}
                 >
-                  {title}
-                </Styled.a>
-              </Styled.h2>
-              <small>{node.date}</small>
-              <Styled.p>{node.excerpt}</Styled.p>
-            </div>
-          </Fragment>
-        );
-      })}
-    </main>
-    <Footer socialLinks={socialLinks} />
-  </Layout>
-);
+                  <Styled.a
+                    as={Link}
+                    css={{
+                      textDecoration: `none`
+                    }}
+                    to={node.slug}
+                  >
+                    {title}
+                  </Styled.a>
+                </Styled.h2>
+                <small>{node.date}</small>
+                <Styled.p>{node.excerpt}</Styled.p>
+              </div>
+            </Fragment>
+          );
+        })}
+      </main>
+      <Footer socialLinks={socialLinks} />
+    </Layout>
+  );
+};
 
 export default Posts;
